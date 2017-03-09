74612e4cb91fa5fcc91a060f3c84a76fde0b98b3
ayleph <ayleph@thisshitistemp.com>
2017-03-09 01:23:39 -0500

    Clean up plugin init code
    Add missing functions and remove unused code.

--- recaptcha/__init__.py
+++ recaptcha/__init__.py
@@ -22,7 +22,7 @@ from mediagoblin.plugins.recaptcha import tools as captcha_tools
 from mediagoblin import messages
 from mediagoblin.tools.translate import lazy_pass_to_ugettext as _
 from mediagoblin.tools import pluginapi
-from mediagoblin.tools import request as request
+from werkzeug.test import create_environ
 from werkzeug.wrappers import Request
 
 _log = logging.getLogger(__name__)
@@ -43,6 +43,17 @@ def setup_plugin():
     pluginapi.register_template_hooks(
         {'captcha_challenge': 'mediagoblin/plugins/recaptcha/captcha_challenge.html'})
 
+    # Create dummy request object to find register_form.
+    environ = create_environ('/foo', 'http://localhost:8080/')
+    request = Request(environ)
+    register_form = pluginapi.hook_handle("auth_get_registration_form", request)
+    del request
+
+    # Add plugin-specific fields to register_form class.
+    register_form_class = register_form.__class__
+    register_form_class.g_recaptcha_response = captcha_forms.RecaptchaHiddenField('reCAPTCHA', id='g-recaptcha-response', name='g-recaptcha-response')
+    register_form_class.remote_address = wtforms.HiddenField('')
+
     _log.info('Done setting up recaptcha!')
 
 
@@ -54,14 +65,13 @@ def transform_registration_form_class(request):
 
 
 def add_to_form_context(context):
+    config = pluginapi.get_config('mediagoblin.plugins.recaptcha')
     context['recaptcha_public_key'] = config.get('RECAPTCHA_PUBLIC_KEY')
     return context
 
 
 hooks = {
     'setup': setup_plugin,
-    'registration_form_transform': transform_registration_form,
-    'registration_form_class_transform': transform_registration_form_class,
     'auth_extra_validation': captcha_tools.extra_validation,
     ('mediagoblin.auth.register',
      'mediagoblin/auth/register.html'): add_to_form_context,
